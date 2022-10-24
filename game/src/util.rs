dba58853a580510467ae8bdad0d8c5e8a3f5f356
jbaker013 <48653149+jbaker013@users.noreply.github.com>
2022-10-24 18:00:03 -0400

    Basic Timer functionality working

--- game/src/util.rs
+++ game/src/util.rs
@@ -127,9 +127,4 @@ impl Object{
 	}
 }
 
-pub fn handle_mouse_clicks(mouse_input: Res<Input<MouseButton>>, windows: Res<Windows>) {
-    let win = windows.get_primary().expect("no primary window");
-    if mouse_input.just_pressed(MouseButton::Left) {
-        println!("click at {:?}", win.cursor_position());
-    }
-}
+
