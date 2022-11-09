290d2165b53f959d405f4883b2a1ccca380cc99a
Jake Salmon <jacobsalmon01@gmail.com>
2022-11-09 16:50:57 -0500

    fixed up breakable objs

--- game/src/physics.rs
+++ game/src/physics.rs
@@ -6,7 +6,7 @@ use bevy::prelude::*;
 use bevy::sprite::collide_aabb::Collision;
 use rand::Rng;
 
-const PROJECTILE_SZ: f32 = 5.;
+const PROJECTILE_SZ: f32 = 6.;
 const PROJECTILE_DAMAGE: i32 = 10;
 
 pub enum ProjType {
@@ -19,6 +19,7 @@ pub enum ProjType {
 pub struct Projectile {
     pub velocity: Vec2,
     pub proj_type: ProjType,
+    pub project_pos: Vec3,
 }
 
 impl Projectile {
@@ -26,12 +27,9 @@ impl Projectile {
         Self {
             velocity: vel,
             proj_type: pt,
+            project_pos: Vec3::splat(0.),
         }
     }
-
-    // pub fn destroy(mut commands: Commands) {
-    //     commands.entity(S).despawn();
-    // }
 }
 
 #[derive(Component)]
@@ -41,9 +39,7 @@ pub struct BrokenObj {
 
 impl BrokenObj {
     pub fn new(lifespan: Timer) -> Self {
-        Self {
-            lifespan: lifespan,
-        }
+        Self { lifespan: lifespan }
     }
 }
 
@@ -63,6 +59,9 @@ pub fn shoot(
     if input.pressed(KeyCode::W) {
         xvel = 0.;
         yvel = 15.;
+    } else if input.pressed(KeyCode::S) {
+        xvel = 0.;
+        yvel = -15.;
     }
     if input.just_pressed(KeyCode::L) {
         // for (pla)
@@ -81,7 +80,6 @@ pub fn shoot(
                 ..default()
             })
             .insert(Projectile::new(Vec2::new(xvel, yvel), ProjType::Projectile));
-        // let id = x.id();
     }
 }
 
@@ -98,31 +96,63 @@ pub fn projectile_collisions(
 ) {
     //let (pl, pt) = player.single_mut();
     for (mut pro_o, mut pro_t, entity) in projectiles.iter_mut() {
-        pro_t.translation.x += pro_o.velocity.x;
-        pro_t.translation.y += pro_o.velocity.y;
+        pro_o.project_pos = Vec3::new(
+            pro_t.translation.x + pro_o.velocity.x,
+            pro_t.translation.y + pro_o.velocity.y,
+            0.,
+        );
+        let mut collide = false;
+        // pro_t.translation.x += pro_o.velocity.x;
+        // pro_t.translation.y += pro_o.velocity.y;
         pro_o.velocity.y += GRAVITY;
         for (mut o_o, o_t, o_e) in objects.iter_mut() {
             let res = bevy::sprite::collide_aabb::collide(
-                Vec3::new(pro_t.translation.x, pro_t.translation.y, 0.),
+                pro_o.project_pos,
                 Vec2::new(PROJECTILE_SZ, PROJECTILE_SZ),
                 o_t.translation,
                 Vec2::new(o_o.width, o_o.height),
             );
-
             if res.is_some() {
-                let mut time: f32 = 10.0;
-                // let entity_o = o_e.id(); // id of object
+                collide = true;
+                let coll_type: bevy::sprite::collide_aabb::Collision = res.unwrap();
+                let mut time: f32 = 5.0;
                 if matches!(o_o.obj_type, ObjectType::Breakable) {
                     commands.entity(o_e).despawn();
-                    for i in 1..10 {
+                    let mut rng = rand::thread_rng();
+                    for i in 1..6 {
                         let mut rng = rand::thread_rng();
-                        let p_xvel = (i as f32 - 5.) / 2.;
-                        let p_yvel = rng.gen_range(3, 6);
+                        let mut p_xvel = 0.;
+                        let mut p_yvel = 0.;
+                        match coll_type {
+                            Collision::Left => {
+                                p_xvel = rng.gen_range(3, 6) as f32;
+                                p_yvel = (i as f32 - 4.) / 2.;
+                            }
+                            Collision::Right => {
+                                p_xvel = rng.gen_range(-6, -3) as f32;
+                                p_yvel = (i as f32 - 4.) / 2.;
+                            }
+                            Collision::Top => {
+                                p_yvel = rng.gen_range(-3, -6) as f32;
+                                p_xvel = (i as f32 - 4.) / 2.;
+                            }
+                            Collision::Bottom => {
+                                p_yvel = rng.gen_range(3, 6) as f32;
+                                p_xvel = (i as f32 - 4.) / 2.;
+                            }
+                            Collision::Inside => {
+                                p_yvel = rng.gen_range(3, 6) as f32;
+                                p_xvel = rng.gen_range(3, 6) as f32;
+                            }
+                        }
+                        // let p_xvel = (i as f32 - 3.) / 2.;
+                        // let p_yvel = rng.gen_range(3, 6);
+                        let sz = o_o.height / rng.gen_range(8, 16) as f32;
                         commands
                             .spawn_bundle(SpriteBundle {
                                 sprite: Sprite {
                                     color: Color::BLACK,
-                                    custom_size: Some(Vec2::new(PROJECTILE_SZ, PROJECTILE_SZ)),
+                                    custom_size: Some(Vec2::new(sz, sz)),
                                     ..default()
                                 },
                                 transform: Transform {
@@ -146,8 +176,6 @@ pub fn projectile_collisions(
                 if matches!(pro_o.proj_type, ProjType::Projectile) {
                     commands.entity(entity).despawn();
                 } else if matches!(pro_o.proj_type, ProjType::BrokenObj) {
-                    let coll_type: bevy::sprite::collide_aabb::Collision = res.unwrap();
-
                     match coll_type {
                         Collision::Left => {
                             pro_o.velocity.x = 0.;
@@ -157,6 +185,9 @@ pub fn projectile_collisions(
                         }
                         Collision::Top => {
                             pro_o.velocity.y = 0.;
+                            pro_o.velocity.x /= 2.;
+                            pro_t.translation.y =
+                                o_t.translation.y + o_o.height / 2. + PROJECTILE_SZ / 2.
                         }
                         Collision::Bottom => {
                             pro_o.velocity.y = 0.;
@@ -164,9 +195,14 @@ pub fn projectile_collisions(
                         Collision::Inside => {
                             pro_o.velocity.x = 0.;
                             pro_o.velocity.y = 0.;
+                            pro_t.translation.y =
+                                o_t.translation.y + o_o.height / 2. + PROJECTILE_SZ / 2.
                         }
                     }
                 }
+            }
+            if !collide {
+                pro_t.translation = pro_o.project_pos;
             }
         }
     }
@@ -175,7 +211,7 @@ pub fn projectile_collisions(
 pub fn despawn_broken_objects(
     time: Res<Time>,
     mut commands: Commands,
-    mut brokenObjects: Query<(&mut BrokenObj, Entity),Without<Object>>,
+    mut brokenObjects: Query<(&mut BrokenObj, Entity), Without<Object>>,
 ) {
     for (mut obj, entity) in brokenObjects.iter_mut() {
         obj.lifespan.tick(time.delta());
@@ -204,7 +240,6 @@ pub fn projectile_active_collision(
 
             if res.is_some() {
                 e_o.health -= PROJECTILE_DAMAGE;
-                println!("{}", e_o.health);
             }
         }
     }
